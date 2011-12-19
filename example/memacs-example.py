30961228ba59401fec83cb20a1058b3ba1faa93b
Armin Wieser <armin.wieser@gmail.com>
2011-12-19 23:51:33 +0100

    PEP8
    Signed-off-by: Armin Wieser <armin.wieser@gmail.com>

--- example/memacs-example.py
+++ example/memacs-example.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 # Time-stamp: <2011-10-28 15:13:31 aw>
 
-import sys, os
+import sys
+import os
 import logging
 # needed to import common.*
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -18,29 +19,32 @@ this class will do ....
 Then an Org-mode file is generated that contains links to the files.
 """
 
+
 class Foo(Memacs):
     def _parser_add_arguments(self):
         Memacs._parser_add_arguments(self)
-        # add additional arguments 
-        
-#        self._parser.add_argument("-f", "--folder", dest="filenametimestamps_folder", action="append", \
-#                        help="path to a folder to search for filenametimestamps, " + 
-#                        "multiple folders can be specified: -f /path1 -f /path2")
-    
-    
+        # add additional arguments
+
+        self._parser.add_argument(
+            "-f", "--folder", dest="filenametimestamps_folder",
+            action="append",
+            help="path to a folder to search for filenametimestamps, " +
+            "multiple folders can be specified: -f /path1 -f /path2")
+
     def _parser_parse_args(self):
         Memacs._parser_parse_args(self)
         # parse additional modules
-    
+
     def _main(self):
-        # do all the stupff 
+        # do all the stupff
         self._writer.write_org_subitem("foo")
         self._writer.write_org_subitem("bar")
 
 if __name__ == "__main__":
-    memacs = Foo(prog_version=PROG_VERSION_NUMBER
-                           , prog_version_date=PROG_VERSION_DATE
-                           , prog_description=PROG_DESCRIPTION
-                           , prog_short_description=PROG_SHORT_DESCRIPTION
-                           , prog_tag=PROG_TAG)
+    memacs = Foo(
+        prog_version=PROG_VERSION_NUMBER,
+        prog_version_date=PROG_VERSION_DATE,
+        prog_description=PROG_DESCRIPTION,
+        prog_short_description=PROG_SHORT_DESCRIPTION,
+        prog_tag=PROG_TAG)
     memacs.handle_main()
