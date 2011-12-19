c3da7f0dbe21f61390b8d93081562e582ff1b756
Armin Wieser <armin.wieser@gmail.com>
2011-12-19 21:59:23 +0100

    refactored example Signed-off-by: Armin Wieser <armin.wieser@gmail.com>

--- example/memacs-example.py
+++ example/memacs-example.py
@@ -3,10 +3,9 @@
 # Time-stamp: <2011-10-28 15:13:31 aw>
 
 import sys, os
-from example.foo import Foo
 # needed to import common.*
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+from common.memacs import Memacs
 
 PROG_VERSION_NUMBER = u"0.0"
 PROG_VERSION_DATE = u"2011-12-18"
@@ -17,6 +16,25 @@ this class will do ....
 
 Then an Org-mode file is generated that contains links to the files.
 """
+
+class Foo(Memacs):
+    def _parser_add_arguments(self):
+        Memacs._parser_add_arguments(self)
+        # add additional arguments 
+        
+#        self._parser.add_argument("-f", "--folder", dest="filenametimestamps_folder", action="append", \
+#                        help="path to a folder to search for filenametimestamps, " + 
+#                        "multiple folders can be specified: -f /path1 -f /path2")
+    
+    
+    def _parser_parse_args(self):
+        Memacs._parser_parse_args(self)
+        # parse additional modules
+    
+    def _main(self):
+        # do all the stupff 
+        self._writer.write_org_subitem("foo")
+        self._writer.write_org_subitem("bar")
 
 if __name__ == "__main__":
     memacs = Foo(prog_version=PROG_VERSION_NUMBER
