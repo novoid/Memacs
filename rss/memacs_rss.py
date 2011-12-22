85a3a13258cf66aba1957e5357d1ac72ee6e26a8
Armin Wieser <armin.wieser@gmail.com>
2011-12-22 23:50:56 +0100

    updated memacs_rss for appending Signed-off-by: Armin Wieser <armin.wieser@gmail.com>

--- rss/memacs_rss.py
+++ rss/memacs_rss.py
@@ -6,6 +6,7 @@ import sys
 import os
 import logging
 import feedparser
+import re
 # needed to import common.*
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from common.reader import CommonReader
@@ -20,12 +21,13 @@ PROG_SHORT_DESCRIPTION = u"Memacs for rss feeds"
 PROG_TAG = u"rss"
 PROG_DESCRIPTION = u"""
 This Memacs module will parse rss files
+RSS2.0 is required
 TODO
 Then to an Org-mode file is generated that contains ...
 """
 
 
-class Foo(Memacs):
+class RssMemacs(Memacs):
     def _parser_add_arguments(self):
         """
         overwritten method of class Memacs
@@ -63,33 +65,54 @@ class Foo(Memacs):
             if not os.access(self._args.file, os.R_OK):
                 self._parser.error("file %s not readable", self._args.file)
 
-    def __get_item_data(self,item):
+    def __get_item_data(self, item):
         """
         
         @return
         """
-        logging.debug(item)
-        properties = OrgProperties()
-        id = item['id']
-        if not id:
-            logging.error("got no id")
-        output = item['title']
-        note = item['description']
-        link = OrgFormat.link(item['link'])
-        note += "\n" + link
-        updated_time_struct = OrgFormat.datetime(item['updated_parsed'])
-        properties.add_property("created", updated_time_struct)
-        properties.add_property("id", id)
+        try:
+            #logging.debug(item)
+            properties = OrgProperties()
+            id = item['id']
+            if not id:
+                logging.error("got no id")
+            output = item['title']
+            link = OrgFormat.link(item['link'])
+            note = link + "\n"
+            note += item['description']
+            updated_time_struct = OrgFormat.datetime(item['updated_parsed'])
+            properties.add_property("created", updated_time_struct)
+            properties.add_property("id", id)
+            
+        except KeyError:
+            logging.error("input is not a RSS 2.0")
+            sys.exit(1)
         
-        dont_parse = ['title','description','link','updated',
-                      'updated_parsed','links']
-        
+        dont_parse = ['title', 'description', 'link', 'updated',
+                          'updated_parsed', 'links']
         for i in  item:
             if i not in dont_parse:
                 if type(i) == unicode and type(item[i]) == unicode:
                     properties.add_property(i, item[i])
 
-        return output, note, properties
+        return output, note, properties, id
+
+    def __get_existing_ids(self):
+        
+        # if we have no outputfile, then we cannot have existing ids
+        if not self._args.outputfile:
+            return []
+        
+        existing_ids = []
+        
+        data = CommonReader.get_data_from_file(self._args.outputfile)
+        for found_id in re.findall(":ID:(.*)", data):
+            found_id = found_id.strip()
+            if found_id != "":
+                existing_ids.append(found_id)
+        
+        logging.debug("there are already %d entries", len(existing_ids))
+        return existing_ids
 
     def _main(self):
         """
@@ -105,15 +128,22 @@ class Foo(Memacs):
         logging.info("title: %s", rss['feed']['title'])
         logging.info("there are: %d entries", len(rss.entries))
         
+        existing_ids = self.__get_existing_ids()
+        
         for item in rss.entries:
-            output, note, properties = self.__get_item_data(item)
-            self._writer.write_org_subitem(output, note=note,properties=properties)
+            output, note, properties, id = self.__get_item_data(item)
+            if id not in existing_ids:
+                logging.debug("appending item to file")
+                self._writer.write_org_subitem(output, note=note, properties=properties)
+            else:
+                logging.debug("not appending item to file")
 
 if __name__ == "__main__":
-    memacs = Foo(
+    memacs = RssMemacs(
         prog_version=PROG_VERSION_NUMBER,
         prog_version_date=PROG_VERSION_DATE,
         prog_description=PROG_DESCRIPTION,
         prog_short_description=PROG_SHORT_DESCRIPTION,
-        prog_tag=PROG_TAG)
+        prog_tag=PROG_TAG,
+        append_orgfile=True)
     memacs.handle_main()
