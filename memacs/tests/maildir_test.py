923338c0ed6d3f1f1563564ded330bd3dbb52e5c
Daniel Fussenegger <d.fussenegger@student.tugraz.at>
2012-05-15 11:03:01 +0200

    added some tests

---
+++ memacs/tests/maildir_test.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Time-stamp: <2012-05-15 17:11:31 df>
+
+import unittest
+import os
+from memacs.maildir import MaildirMemacs
+
+
+class TestTagstore(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        
+    def test_from_file(self):    
+        test_file = os.path.dirname(
+            os.path.abspath(__file__)) + os.sep + "tmp" \
+            + os.sep + "maildir"
+             
+        argv = []      
+        argv.append("-fp")
+        argv.append(test_file)
+
+        memacs = MaildirMemacs(argv=argv)
+        data = memacs.test_get_entries()
+
+        self.assertEqual(
+            data[0],
+            '** <2007-11-19 Mon 09:28:31> [[mailto:test.test@test.at]["test.test@test.at"]]: SomeSubject')
+        self.assertEqual(
+            data[1],
+            "   :PROPERTIES:")
+        self.assertEqual(
+            data[3],
+            '   :ID:         <200711190928.lAJ9SVJr026663@fstgss22..at>')
+        self.assertEqual(
+            data[4],
+            "   :END:")
+
+        def tearDown(self):
+            pass
---
+++ memacs/tests/mbox_test.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Time-stamp: <2012-05-15 17:11:31 df>
+
+import unittest
+import os
+from memacs.mbox import MboxMemacs
+
+
+class TestTagstore(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        
+    def test_from_file(self):    
+        test_file = os.path.dirname(
+            os.path.abspath(__file__)) + os.sep + "tmp" \
+            + os.sep + "mboxexample"
+             
+        argv = []      
+        argv.append("-nf")
+        argv.append(test_file)
+
+        memacs = MboxMemacs(argv=argv)
+        data = memacs.test_get_entries()
+
+        self.assertEqual(
+            data[0],
+            "** <2012-02-20 Mon 12:27:50> [[mailto:foo.bob@gmail.com][Foo Bob]]@[[news:tu-graz.flames][tu-graz.flames]]: Re: Thats a Test")
+        self.assertEqual(
+            data[1],
+            "   :PROPERTIES:")
+        self.assertEqual(
+            data[3],
+            "   :ID:         <foo.bob@gmail.com>")
+        self.assertEqual(
+            data[4],
+            "   :END:")
+
+        def tearDown(self):
+            pass
