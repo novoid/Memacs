3089310b41e77543b234d4769c45f992db5666f4
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-01-02 18:12:23 +0530

    Fix test errors in rss_test
    - Path was hard-coded.
    - Seconds issue.

--- memacs/tests/photos_test.py
+++ memacs/tests/photos_test.py
@@ -9,27 +9,23 @@ from memacs.photos import PhotosMemacs
 class TestPhotoMemacs(unittest.TestCase):
 
     def test_from_file(self):
-        test_path = os.path.dirname(os.path.abspath(__file__)) + \
-            os.sep + "tmp"
+        test_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'tmp'
+        )
         argv = "-s -f " + test_path
         memacs = PhotosMemacs(argv=argv.split())
         data = memacs.test_get_entries()
 
-        # generate assertEquals :)
-#        for d in range(len(data)):
-#            print "self.assertEqual(\n\tdata[%d],\n\t \"%s\")" % \
-#               (d, data[d])
+        filename = 'fujifilm-finepix40i.jpg'
+        path = os.path.join(test_path, filename)
 
         self.assertEqual(
             data[0],
-             u"** <2000-08-04 Fri 18:22> [[/home/vk/src/memacs/mema" + \
-             "cs/tests/tmp/fujifilm-finepix40i.jpg][fujifilm-finepix40i.jpg]]")
-        self.assertEqual(
-            data[1],
-             "   :PROPERTIES:")
+            "** <2000-08-04 Fri 18:22> [[%s][%s]]" % (path, filename)
+        )
+        self.assertEqual(data[1], "   :PROPERTIES:")
         self.assertEqual(
             data[2],
-             u"   :ID:         c2833ac1c683dea5b600ac4f303a572d2148e1e7")
-        self.assertEqual(
-            data[3],
-             "   :END:")
+            u"   :ID:         c2833ac1c683dea5b600ac4f303a572d2148e1e7"
+        )
+        self.assertEqual(data[3], "   :END:")
