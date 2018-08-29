b0b7b110f10e311c9a79dd5803bb65cfa14a6f97
Max Beutelspacher <max.beutelspacher@mailbox.org>
2018-08-29 21:10:41 +0200

    change ID's in csv tests

--- memacs/tests/csv_test.py
+++ memacs/tests/csv_test.py
@@ -5,7 +5,6 @@ import unittest
 import os
 from memacs.csv import Csv
 
-
 class TestCsv(unittest.TestCase):
 
     def test_example1(self):
@@ -34,7 +33,7 @@ class TestCsv(unittest.TestCase):
         self.assertEqual(data[1], "   :PROPERTIES:")
         self.assertEqual(data[2], "   :CURRENCY:   EUR")
         self.assertEqual(data[3], "   :VALUE:      100,00")
-        self.assertEqual(data[4], "   :ID:         3f4898135bc340ede51aff4519ebd54db92fe23c")
+        self.assertEqual(data[4], "   :ID:         a9cfa86bd9d89b72f35bfca0bb75131be0ca86b1")
         self.assertEqual(data[5], "   :END:")
 
     def test_example2(self):
@@ -57,5 +56,5 @@ class TestCsv(unittest.TestCase):
 
         self.assertEqual(data[0], "** <2012-02-23 Thu 14:40> Alibaba")
         self.assertEqual(data[1], "   :PROPERTIES:")
-        self.assertEqual(data[2], "   :ID:         08cfc2bf06e9f2a235641912ea7c7d7c87072ad3")
+        self.assertEqual(data[2], "   :ID:         b1a598a360d4766cf94822cdcd96629f0b3936ef")
         self.assertEqual(data[3], "   :END:")
