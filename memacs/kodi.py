586034a4a174129a60e5b9eb45360a6e99e18c02
Max Beutelspacher <max.beutelspacher@mailbox.org>
2018-10-27 15:46:50 +0200

    implement minimal pause duration

--- memacs/kodi.py
+++ memacs/kodi.py
@@ -17,10 +17,11 @@ from itertools import tee, islice, chain
 
 
 # stolen from https://stackoverflow.com/questions/1011938/python-previous-and-next-values-inside-a-loop/1012089#1012089
-def previous_and_current(some_iterable):
-    prevs, items = tee(some_iterable, 2)
+def previous_current_next(some_iterable):
+    prevs, items, nexts = tee(some_iterable, 3)
     prevs = chain([None], prevs)
-    return zip(prevs, items)
+    nexts = chain(islice(nexts, 1, None), [None])
+    return zip(prevs, items, nexts)
 
 
 class Kodi(Csv):
@@ -40,6 +41,38 @@ class Kodi(Csv):
             help='field name of the action (start/paused,stopped)',
             type=str.lower)
 
+        self._parser.add_argument(
+            '--identification-fields',
+            dest="identification_fields",
+            required=True,
+            action='store',
+            help='field names to uniquely identify one track e.g. title,artist',
+            type=str.lower)
+
+        self._parser.add_argument(
+            '--minimal-pause-duration',
+            dest='minimal_pause_duration',
+            required=False,
+            action='store',
+            default=0,
+            help=
+            'minimal duration in seconds of a pause to be logged as a pause instead of being ignored',
+            type=int,
+        )
+
+    def _parser_parse_args(self):
+        """
+        overwritten method of class Memacs
+
+        all additional arguments are parsed in here
+        """
+        super()._parser_parse_args()
+
+        if self._args.identification_fields:
+            self._args.identification_fields = [
+                name.strip()
+                for name in self._args.identification_fields.split(',')
+            ]
 
     def read_timestamp(self, row):
         if not self._args.timestamp_format:
@@ -49,6 +82,9 @@ class Kodi(Csv):
             timestamp = time.strptime(row[self._args.timestamp_field],
                                       self._args.timestamp_format)
 
+        return timestamp
+
+    def format_timestamp(self, timestamp):
         # show time with the timestamp format, but only
         # if it contains at least hours and minutes
         if not self._args.timestamp_format or \
@@ -71,29 +107,44 @@ class Kodi(Csv):
         properties = self.read_properties(row)
         output = self._args.output_format.format(**row)
         self._writer.write_org_subitem(
-            timestamp=start_time + '--' + stop_time,
+            timestamp=self.format_timestamp(start_time) + '--' +
+            self.format_timestamp(stop_time),
             output=output,
             properties=properties)
+
+    def tracks_are_identical(self, row1, row2):
+        for field in self._args.identification_fields:
+            if row1[field] != row2[field]:
+                return False
+        return True
+
+    def track_is_paused(self, row, next_row):
+        return next_row and self.tracks_are_identical(row, next_row) and (
+            self.read_timestamp(next_row) - self.read_timestamp(row)
+        ).total_seconds() < self._args.minimal_pause_duration
 
     def read_log(self, reader):
         """goes through rows and searches for start/stop actions"""
         start_actions = ['started', 'resumed']
         stop_actions = ['paused', 'stopped']
         start_time, stop_time = None, None
-        for prev_row, row in previous_and_current(reader):
+        for prev_row, row, next_row in previous_current_next(reader):
             timestamp = self.read_timestamp(row)
             action = row[self._args.action_field]
             if action in start_actions:
                 if not start_time:
                     start_time = timestamp
-                else:
-                    if prev_row:
-                        self.write_one_track(prev_row, start_time, timestamp)
+                elif prev_row and not self.track_is_paused(prev_row, row):
+                    self.write_one_track(prev_row, start_time, timestamp)
                     start_time = timestamp
             elif action in stop_actions and start_time:
-                stop_time = timestamp
+                if not self.track_is_paused(row, next_row):
+                    stop_time = timestamp
+                else:
+                    stop_time = None
             if start_time and stop_time:
-                self.write_one_track(row, start_time, stop_time)
+                if self.tracks_are_identical(row, prev_row):
+                    self.write_one_track(row, start_time, stop_time)
                 start_time, stop_time = None, None
 
     def _main(self):
