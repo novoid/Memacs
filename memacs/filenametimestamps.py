2512e1582463a9f09732dfe72bba94de51b281b1
Karl Voit <git@Karl-Voit.at>
2019-10-02 19:00:20 +0200

    filenametimestamps: some PEP8 corrections + logging

--- memacs/filenametimestamps.py
+++ memacs/filenametimestamps.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2014-03-13 17:32:22 karl.voit>
+# Time-stamp: <2019-10-02 18:59:27 vk>
 
 import os
 from memacs.lib.memacs import Memacs
@@ -14,7 +14,7 @@ import sys
 import codecs
 
 DATESTAMP_REGEX = re.compile("([12]\d{3})-([01]\d)-([0123]\d)")
-TIMESTAMP_REGEX = re.compile("([12]\d{3})-([01]\d)-([0123]\d)T([012]\d)" + \
+TIMESTAMP_REGEX = re.compile("([12]\d{3})-([01]\d)-([0123]\d)T([012]\d)" +
                              "[.]([012345]\d)([.]([012345]\d))?")
 
 
@@ -26,18 +26,18 @@ class FileNameTimeStamps(Memacs):
         self._parser.add_argument("-f", "--folder",
                                   dest="filenametimestamps_folder",
                                   action="append",
-                                  help="path to a folder to search for " + \
-                                       "filenametimestamps, " + \
-                                       "multiple folders can be specified: " + \
+                                  help="path to a folder to search for " +
+                                       "filenametimestamps, " +
+                                       "multiple folders can be specified: " +
                                        "-f /path1 -f /path2")
 
         self._parser.add_argument("-x", "--exclude", dest="exclude_folder",
-                        help="path to excluding folder, for more excludes " + \
-                             "use this: -x /path/exclude -x /path/exclude")
+                                  help="path to excluding folder, for more excludes " +
+                                  "use this: -x /path/exclude -x /path/exclude")
 
         self._parser.add_argument("--filelist", dest="filelist",
-                        help="file containing a list of files to process. " + \
-                             "either use \"--folder\" or the \"--filelist\" argument, not both.")
+                                  help="file containing a list of files to process. " +
+                                  "either use \"--folder\" or the \"--filelist\" argument, not both.")
 
         self._parser.add_argument("--ignore-non-existing-items",
                                   dest="ignore_nonexisting", action="store_true",
@@ -45,23 +45,23 @@ class FileNameTimeStamps(Memacs):
 
         self._parser.add_argument("-l", "--follow-links",
                                   dest="follow_links", action="store_true",
-                                  help="follow symbolics links," + \
-                                      " default False")
+                                  help="follow symbolics links," +
+                                  " default False")
 
         self._parser.add_argument("--skip-file-time-extraction",
                                   dest="skip_filetime_extraction",
                                   action="store_true",
-                                  help="skip extraction of the file time " + \
-                                  " in files containing only the date in " + \
+                                  help="skip extraction of the file time " +
+                                  "in files containing only the date in " +
                                   "the filename")
 
         self._parser.add_argument("--force-file-date-extraction",
                                   dest="force_filedate_extraction",
                                   action="store_true",
-                                  help="force extraction of the file date" + \
-                                  " even if there is no ISO datestamp in " + \
-                                  "the filename"
-        )
+                                  help="force extraction of the file date" +
+                                  " even if there is no ISO datestamp in " +
+                                  "the filename")
+
 
     def _parser_parse_args(self):
         Memacs._parser_parse_args(self)
@@ -74,14 +74,14 @@ class FileNameTimeStamps(Memacs):
 
         if self._args.filelist:
             if not os.path.isfile(self._args.filelist):
-                self._parser.error("Check the filelist argument: " + \
-                                       "[" + str(self._args.filelist) + "] is not an existing file")
+                self._parser.error("Check the filelist argument: " +
+                                   "[" + str(self._args.filelist) + "] is not an existing file")
 
         if self._args.filenametimestamps_folder:
             for f in self._args.filenametimestamps_folder:
                 if not os.path.isdir(f):
-                    self._parser.error("Check the folderlist argument: " + \
-                                           "[" + str(f) + "] and probably more aren't folders")
+                    self._parser.error("Check the folderlist argument: " +
+                                       "[" + str(f) + "] and probably more aren't folders")
 
     def __ignore_dir(self, ignore_dir):
         """
@@ -90,7 +90,7 @@ class FileNameTimeStamps(Memacs):
                        false - otherwise
         """
         if self._args.exclude_folder and \
-        ignore_dir in self._args.exclude_folder:
+           ignore_dir in self._args.exclude_folder:
             logging.info("ignoring ignore_dir: " + ignore_dir)
             return True
         else:
@@ -101,7 +101,7 @@ class FileNameTimeStamps(Memacs):
         walks through a folder
         """
         for rootdir, dirs, files in os.walk(folder,
-                                        followlinks=self._args.follow_links):
+                                            followlinks=self._args.follow_links):
             if not self.__ignore_dir(rootdir):
                 for file in files:
                     self.__handle_file(file, rootdir)
@@ -124,18 +124,18 @@ class FileNameTimeStamps(Memacs):
             orgdate = OrgFormat.strdate(datestamp)
             orgdate_time_tupel = OrgFormat.datetupeliso8601(datestamp)
 
-            if self._args.skip_filetime_extraction != True:            
+            if not self._args.skip_filetime_extraction:
 
                 if os.path.exists(link):
                     file_datetime = time.localtime(os.path.getmtime(link))
                     # check if the file - time information matches year,month,day,
                     # then update time
                     if file_datetime.tm_year == orgdate_time_tupel.tm_year and \
-                        file_datetime.tm_mon == orgdate_time_tupel.tm_mon and \
-                        file_datetime.tm_mday == orgdate_time_tupel.tm_mday:
-                  
+                       file_datetime.tm_mon == orgdate_time_tupel.tm_mon and \
+                       file_datetime.tm_mday == orgdate_time_tupel.tm_mday:
+
                         logging.debug("found a time in file.setting %s-->%s",
-                                    orgdate, OrgFormat.date(file_datetime, True))
+                                      orgdate, OrgFormat.date(file_datetime, True))
                         orgdate = OrgFormat.date(file_datetime, True)
                 else:
                     logging.debug("item [%s] not found and thus could not determine mtime" % link)
@@ -156,7 +156,7 @@ class FileNameTimeStamps(Memacs):
 
     def __handle_file(self, file, rootdir):
         """
-        handles a file
+        handles a file (except ending with a tilde)
         """
         # don't handle emacs tmp files (file~)
         if file[-1:] == '~':
@@ -183,7 +183,8 @@ class FileNameTimeStamps(Memacs):
                 # an Exception TimestampParseException, which is thrown,
                 # wen strptime (parse from string to time tupel) fails
                 self.__parse_file(file, link)
-            except TimestampParseException as e:
+            except TimestampParseException:
+                logging.debug('__parse_filename_iso_timestamp() caused an TimestampParseException')
                 logging.warning("False date(time) in file: %s", link)
 
     def _main(self):
